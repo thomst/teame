b33b9c6378030a86a1ab142bf27a20e9340047f2
Thomas Leichtfuß <leichtfuss@systopia.de>
2023-11-09 14:59:22 +0100

    added teame.sh and README.md

---
+++ README.md
@@ -0,0 +1,133 @@
+Teame
+=====
+
+Teame is a tool to clone or pull repositories from a gitea server in parallelized batch mode. Using [tea](https://gitea.com/gitea/tea) to access the gitea api.
+
+
+Content
+-------
+
+- [Installation](#installation)
+- [Setup](#setup)
+- [Usage](#usage)
+- [Examples](#examples)
+- [Feedback](#feedback)
+
+
+Installation
+------------
+
+### tea
+
+*   Download binary from [here](https://dl.gitea.com/tea/0.9.2/tea-0.9.2-linux-amd64).
+*   Make it executable.
+*   Move it anywhere on your bin path.
+*   Create a symlink.
+
+```
+wget https://dl.gitea.com/tea/0.9.2/tea-0.9.2-linux-amd64
+chmod +x tea-0.9.2-linux-amd64
+sudo mv tea-0.9.2-linux-amd64 /usr/local/bin/
+cd /usr/local/bin/
+sudo ln -s tea-0.9.2-linux-amd64 tea
+
+```
+
+Alternatively just follow the installation instructions [here](https://gitea.com/gitea/tea#installation).
+
+### teame.sh
+
+Just put teame.sh anywhere on your bin path and make it executable.
+
+```
+clone https://github.com/thomst/teame.git
+sudo ln -s teame/teame.sh /usr/local/bin/teame
+```
+
+Setup
+-----
+
+*   Create a gitea access token as described [here](https://docs.gitea.com/next/development/api-usage#generating-and-listing-api-tokens).
+*   Add a tea login using `tea login add`.
+
+
+Usage
+-----
+
+Use `teame -h`:
+
+```
+description:
+    Clone or pull gitea repositories.
+
+usage:
+    tea-me.sh -h
+    tea-me.sh [-l][-d DIR][-u LOGIN][-p COUNT] PATTERN
+
+Get or update repositories from gitea.
+
+actions:
+    -l              list repositories
+    -g              get or update repositories
+    -s              print status of repositories
+    -h              print help-message
+
+options:
+    -d [DIR]        working directory (default: current working directory)
+    -u [LOGIN]      tea login (default: current unix user)
+    -p [COUNT]      count of parallel processes (default: 8)
+```
+
+
+Examples
+--------
+
+### List repositories filtered by 'my-pattern'
+
+
+```
+$ **teame -l my-pattern**
+repo-with-my-pattern-in-name
+another-repo-with-my-pattern-in-name
+```
+
+#### Explanation:
+
+Teame uses `tea repos search` to get a list of repositories filtered by the search pattern.
+
+
+### Clone or pull repositories
+
+```
+$ **teame.sh -g my-pattern**
+[SUCCESS][PULLED] repo-with-my-pattern-in-name
+[SUCCESS][CLONED] another-repo-with-my-pattern-in-name
+```
+
+#### Explanation
+
+*   Teame gets a list of repositories from the gitea server filtered by the search pattern.
+*   And then depending on the fact that the repository already exists in the current working directory teame either
+    *   clones the repository or
+    *   pulls it.
+
+
+### Show repository status
+
+```
+$ **teame.sh -s my-pattern**
+[SUCCESS][STATUS] repo-with-my-pattern-in-name
+ D deleted-file
+?? new-file
+[SUCCESS][CLEAN] another-repo-with-my-pattern-in-name
+```
+
+#### Explanation
+
+Teame lists the status of each repository using a machine readable format.
+
+
+Feedback
+--------
+
+Any feedback welcome: leichtfuss@systopia.de
---
+++ teame.sh
@@ -0,0 +1,241 @@
+#!/bin/bash
+
+USAGE="description:
+    Clone or pull gitea repositories.
+
+usage:
+    tea-me.sh -h
+    tea-me.sh [-l][-d DIR][-u LOGIN][-p COUNT] PATTERN
+
+Get or update repositories from gitea.
+
+actions:
+    -l              list repositories
+    -g              get or update repositories
+    -s              print status of repositories
+    -h              print help-message
+
+options:
+    -d [DIR]        working directory (default: current working directory)
+    -u [LOGIN]      tea login (default: current unix user)
+    -p [COUNT]      count of parallel processes (default: 8)
+"
+
+while getopts lgsd:u:p:h opt
+do
+    case $opt in
+        l)  LIST_REPO=true;;
+        g)  GET_OR_UPDATE_REPO=true;;
+        s)  GET_REPO_STATUS=true;;
+        d)  CWD=$OPTARG;;
+        u)  LOGIN=$OPTARG;;
+        p)  PROCESS_POOL=$OPTARG;;
+        h)  echo "$USAGE"; exit 0;;
+        \?) echo "$USAGE"; exit 1;;
+        :)  echo "$USAGE"; exit 1;;
+    esac
+done
+
+
+#-------------------------------------------------------------------------------
+# Set defaults for our runtime parameters.
+#-------------------------------------------------------------------------------
+PATTERN="${*:$OPTIND:1}"
+CWD="${CWD:-"$(pwd)"}"
+LOGIN="${LOGIN:-"$(id --user --name)"}"
+PROCESS_POOL="${PROCESS_POOL:-8}"
+
+#-------------------------------------------------------------------------------
+# Set some useful global variables.
+#-------------------------------------------------------------------------------
+RTE_PIDS=()
+MSG_ERROR="\r\e[31m[ERROR]\e[0m"
+MSG_SUCCESS="\r\e[32m[SUCCESS]\e[0m"
+MSG_PULLED="\e[33m[PULLED]\e[0m"
+MSG_CLONED="\e[33m[CLONED]\e[0m"
+MSG_STATUS="\e[33m[STATUS]\e[0m"
+MSG_CLEAN="\e[33m[CLEAN]\e[0m"
+
+
+# FUNCTION ---------------------------------------------------------------------
+# NAME:         get_list_of_repositories
+# DESCRIPTION:  Retrieve a list of repository names and ssh adresses filtered by
+#               a pattern from the gitea api via tea.
+# PARAMETER 1:  [str] search pattern
+# OUTPUT:       list of repository name and address seperated by a whitespace.
+# ------------------------------------------------------------------------------
+function get_list_of_repositories {
+    pattern=$1
+    repos=()
+    index=0
+    while true; do
+        index=$((index+1))
+        mapfile -t page < <(tea repos search --login="$LOGIN" --page=$index --output=simple --fields=name,ssh "$pattern")
+        if [ -n "${page[*]}" ]; then
+            repos+=("${page[@]}")
+        else
+            break
+        fi
+    done
+    if [ -n "${repos[*]}" ]; then
+        (IFS=$'\n'; echo "${repos[*]}")
+    fi
+}
+
+
+# FUNCTION ---------------------------------------------------------------------
+# NAME:         update_repository
+# DESCRIPTION:  Update a repository found in the current working directory by
+#               pulling from the remote repository.
+# PARAMETER 1:  [str] repository name
+# OUTPUT:       Print informations about what has been done.
+# ------------------------------------------------------------------------------
+function update_repository {
+    name="$1"
+    if [ ! -d "$CWD/$name/.git" ]; then
+        echo -e "${MSG_ERROR}${MSG_PULLED}[$name] missing repository"
+        return
+    fi
+    pushd "$CWD/$name" > /dev/null || exit
+    if output="$(git pull 2>&1)"; then
+        echo -e "${MSG_SUCCESS}${MSG_PULLED} $name"
+    else
+        echo -e "${MSG_ERROR}${MSG_PULLED} $name"
+        echo "$output"
+    fi
+    popd > /dev/null || exit
+}
+
+
+# FUNCTION ---------------------------------------------------------------------
+# NAME:         clone_repository
+# DESCRIPTION:  Clone a repository into the current working directory.
+# PARAMETER 1:  [str] repository name
+# PARAMETER 2:  [str] repository ssh address
+# OUTPUT:       Print informations about what has been done.
+# ------------------------------------------------------------------------------
+function clone_repository {
+    name="$1"
+    ssh_address="$2"
+    if [ -d "$CWD/$name/.git" ]; then
+        echo -e "${MSG_ERROR}${MSG_CLONED}[$name] repository already exists"
+        return
+    fi
+    pushd "$CWD" > /dev/null || exit
+    if output="$(git clone "$ssh_address" 2>&1)"; then
+        echo -e "${MSG_SUCCESS}${MSG_CLONED} $name"
+    else
+        echo -e "${MSG_ERROR}${MSG_CLONED} $name"
+        echo "$output"
+    fi
+    popd > /dev/null || exit
+}
+
+
+# FUNCTION ---------------------------------------------------------------------
+# NAME:         get_repository_status
+# DESCRIPTION:  Print repository status in machine readable format.
+# PARAMETER 1:  [str] repository name
+# OUTPUT:       Print informations about what has been done.
+# ------------------------------------------------------------------------------
+function get_repository_status {
+    name="$1"
+    if [ ! -d "$CWD/$name/.git" ]; then
+        echo -e "${MSG_ERROR}${MSG_STATUS}[$name] missing repository"
+        return
+    fi
+    pushd "$CWD/$name" > /dev/null || exit
+    if output="$(git status --porcelain 2>&1)"; then
+        if [ -n "$output" ]; then
+            echo -e "${MSG_SUCCESS}${MSG_STATUS} $name"
+            echo "$output"
+        else
+            echo -e "${MSG_SUCCESS}${MSG_CLEAN} $name"
+        fi
+    else
+        echo -e "${MSG_ERROR}${MSG_STATUS} $name"
+        echo "$output"
+    fi
+    popd > /dev/null || exit
+}
+
+
+# FUNCTION ---------------------------------------------------------------------
+# NAME:         get_or_update_repository
+# DESCRIPTION:  Call update_repository or clone_repository depending on if the
+#               repository already exists in the current working directory or
+#               not.
+# PARAMETER 1:  [str] repository name
+# PARAMETER 2:  [str] repository ssh address
+# ------------------------------------------------------------------------------
+function get_or_update_repository {
+    name="$1"
+    ssh_address="$2"
+    if [ -d "$CWD/$name/.git" ]; then
+        update_repository "$name"
+    else
+        clone_repository "$name" "$ssh_address"
+    fi
+}
+
+
+# FUNCTION ---------------------------------------------------------------------
+# NAME:         process_repositories
+# DESCRIPTION:  Reading a list of repository name and ssh address pairs from
+#               stdin and processing this list line by line in parallelized
+#               background processes.
+# OUTPUT:       Print the output of the action that was performed for each
+#               repository.
+# ------------------------------------------------------------------------------
+function process_repositories {
+    while [ -z "$RTE_LOOP_STOP" ] && read -r line; do
+        name="${line% *}"
+        ssh_address="${line#* }"
+
+        #-----------------------------------------------------------------------
+        # Process repositories within a background process for parallelization.
+        #-----------------------------------------------------------------------
+        (
+            if [ -n "$GET_OR_UPDATE_REPO" ]; then
+                output="$(get_or_update_repository "$name" "$ssh_address" 2>&1)"
+            elif [ -n "$GET_REPO_STATUS" ]; then
+                output="$(get_repository_status "$name"  2>&1)"
+            fi
+            echo -e "$output"
+        ) &
+
+        #-----------------------------------------------------------------------
+        # Save the process ids within a variable and setup a trap command.
+        #-----------------------------------------------------------------------
+        RTE_PIDS+=( $! )
+        trap 'RTE_LOOP_STOP=true; kill -s TERM ${RTE_PIDS[*]} 2> /dev/null' SIGINT
+
+        #-----------------------------------------------------------------------
+        # Wait till there are less than PROCESS_POOL processes running.
+        #-----------------------------------------------------------------------
+        while (( "$(ps --no-headers --format pid -p "${RTE_PIDS[*]}" | wc -l)" >= "$PROCESS_POOL" )); do
+            sleep 0.2
+        done
+
+        #-----------------------------------------------------------------------
+        # Give the processes a little space.
+        #-----------------------------------------------------------------------
+        sleep 0.2
+
+    done < "/dev/stdin"
+
+    #---------------------------------------------------------------------------
+    # Wait for background-processes.
+    #---------------------------------------------------------------------------
+    [[ -n "${RTE_PIDS[*]}" ]] && wait ${RTE_PIDS[*]}
+}
+
+
+#-------------------------------------------------------------------------------
+# Run teame.
+#-------------------------------------------------------------------------------
+if [ -n "$LIST_REPO" ]; then
+    get_list_of_repositories "$PATTERN" | cut --fields=1 --delimiter=' '
+elif [ -n "$GET_OR_UPDATE_REPO" ] || [ -n "$GET_REPO_STATUS" ]; then
+    get_list_of_repositories "$PATTERN" | process_repositories
+fi
